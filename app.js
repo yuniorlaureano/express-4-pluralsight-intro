4b41e5a3df29e372300bb7efabb69d30d92addc0
Yunior Miguel Laureano <yu.laureano@dga.gov.do>
2019-04-23 09:05:49 -0400

    Scripts autotation, nodemon, eslint, es6

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
 var path = require('path');
+var morgan = require('morgan');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
@@ -14,6 +17,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/views/', '/index.html'));
 });
 
-app.listen(3000, function() {
-    console.log('listeing on port ${chalk.green(3000)}');
+app.listen(port, function() {
+    debug('listeing on port ${chalk.green(3000)}');
 });
