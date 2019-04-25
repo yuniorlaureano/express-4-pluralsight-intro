556f588cab6205ee371908f63b7b86b2a094ac4c
Yunior Miguel Laureano <yu.laureano@dga.gov.do>
2019-04-25 09:03:57 -0400

    View engine pug, router, and separte router file

--- app.js
+++ app.js
@@ -12,9 +12,15 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
+const bookRouter = require('./src/routes/bookRoutes')('route info');
+
+app.use('/books', bookRouter);
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/views/', '/index.html'));
+    //res.sendFile(path.join(__dirname, '/views/', '/index.html'));
+    res.render('index', { title: "MyApp", list: ['a', 'b'] });
 });
 
 app.listen(port, function() {
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+const bookRouter = express.Router();
+
+
+function router(info) {
+    bookRouter.route('/').get((req, res) => {
+        res.render('group', { title: "MyApp", list: ['a', 'b'] });
+    });
+
+    bookRouter.route('/:id').get((req, res) => {
+        const { id } = req.params;
+        res.render('single', { title: "MyApp", list: ['a', 'b'] });
+    });
+}
+
+
+module.exports = router;
